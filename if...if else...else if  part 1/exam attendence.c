ed63b92b0b55eddabb2885b4f33a37235d6eab08
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-02-11 21:51:53 +0530

    Create exam attendence.c

---
+++ if...if else...else if  part 1/exam attendence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int attened;
+    scanf("%d", &attend);
+    if (attend>= 75)
+        printf("Eligible for Exam");
+    else
+        printf("Not Eligible");
+
+    return 0;
+}
