9a21840a3d26b65d6398039b3bf48a843aa0cd0d
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-02-11 21:46:47 +0530

    Create vechicle speed.c

---
+++ if...if else...else if  part 1/vechicle speed.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int s;
+    scanf("%d", &s);
+    if (s > 80)
+        printf("Speed Limit ");
+    else
+        printf("Within Speed Limit");
+
+    return 0;
+}
