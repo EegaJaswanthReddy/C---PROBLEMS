82c0a72ba220a9f11fa3961ec852889b78d9a0e1
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-02-15 15:39:17 +0530

    Create mobile warranty.c

---
+++ switch case.c/mobile warranty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int warranty;
+    scanf("%d", &warranty);
+    switch (warranty) {
+        case 1:
+            printf("Under Warranty");
+            break;
+        case 2:
+            printf("Limited Warranty");
+            break;
+        case 3:
+            printf("Out of Warranty");
+            break;
+        default:
+            printf("Invalid Input");
+    }
+    return 0;
+}
