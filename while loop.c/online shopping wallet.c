ac5d896f7fcacc32977bd8135613073ef95d51f4
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-03-07 15:57:21 +0530

    Create online shopping wallet.c

---
+++ while loop.c/online shopping wallet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int walletBalance, N;
+    int purchase;
+    int i = 0;
+    int success = 0;
+    scanf("%d", &walletBalance);
+    scanf("%d", &N);
+    while (i < N) {
+        scanf("%d", &purchase);
+
+        if (walletBalance >= purchase) {
+            walletBalance = walletBalance - purchase;
+            success++;
+        } else {
+            break;
+        }
+        i++;
+    }
+    printf("Successful Purchases: %d\n", success);
+    printf("Final Balance: %d\n", walletBalance);
+
+    return 0;
+}
