d3b42f388719aea8a7c04e243474c81a243c0d7d
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-03-12 22:27:18 +0530

    Create Verify sorted daily.c

---
+++ 1d array.c/Verify sorted daily.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, a[100];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n-1; i++) {
+        if(a[i] > a[i+1]) {
+            printf("No");
+            return 0;
+        }
+    }
+
+    printf("Yes");
+    return 0;
+}
