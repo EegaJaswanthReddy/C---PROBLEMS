a0641691589ac77895f5b4e63e9c19dfb3f44935
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-03-15 20:45:51 +0530

    Create Count the even and odd.c

---
+++ 1d array.c/Count the even and odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, num, even = 0, odd = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num);
+
+        if(num % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even:%d Odd:%d", even, odd);
+
+    return 0;
+}
