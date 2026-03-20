71b28d41e3300f50e60ab78ca5a4af5ea8c28fbb
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-03-20 21:08:46 +0530

    Create Expense balance point.c

---
+++ 1d array.c/Expense balance point.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int total = 0;
+    for(int i = 0; i < n; i++) {
+        total += arr[i];
+    }
+    int left = 0;
+    for(int i = 0; i < n; i++) {
+        int right = total - left - arr[i];
+        if(left == right) {
+            printf("%d", i);
+            return 0;
+        }
+        left += arr[i];
+    }
+    printf("-1");
+    return 0;
+}
