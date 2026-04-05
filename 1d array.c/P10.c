d46973d6d0dc7660b7a8e643e073a47985af62a7
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-04-05 22:20:13 +0530

    Create P10.c

---
+++ 1d array.c/P10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+
+    scanf("%d %d", &R, &C);
+
+    int data[R][C];
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &data[i][j]);
+        }
+    }
+
+    int maxRow = 0;
+    long long maxProduct = 1;
+
+    for(int j = 0; j < C; j++) {
+        maxProduct *= data[0][j];
+    }
+
+    for(int i = 1; i < R; i++) {
+        long long product = 1;
+
+        for(int j = 0; j < C; j++) {
+            product *= data[i][j];
+        }
+
+        if(product > maxProduct) {
+            maxProduct = product;
+            maxRow = i;
+        }
+    }
+
+    printf("%d\n", maxRow);
+
+    return 0;
+}
