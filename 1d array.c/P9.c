ee4497cbbfaffcb3cb28f34d087058de9e28a5d3
EegaJaswanthReddy <jaswanthreddyeega2468@gmail.com>
2026-04-07 20:13:52 +0530

    Create P9.c

---
+++ 1d array.c/P9.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    
+    scanf("%d %d", &R, &C);
+
+    int data[R][C];
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &data[i][j]);
+        }
+    }
+
+    int maxProduct = 1;
+    int maxRow = 0;
+
+    for(int i = 0; i < R; i++) {
+        int product = 1;
+
+        for(int j = 0; j < C; j++) {
+            product *= data[i][j];
+        }
+
+        if(i == 0 || product > maxProduct) {
+            maxProduct = product;
+            maxRow = i;
+        }
+    }
+
+
+      printf("%d", maxRow);
+
+    return 0;
+}
